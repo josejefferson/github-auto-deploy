223468842e4fd7510bac5bd20b687dcdb0a59543
Jefferson Dantas <jeffersonn.jjdaj@gmail.com>
2023-01-12 13:29:51 +0000

    feat: date in logs

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -22,9 +22,16 @@ export function random(min: number, max: number) {
 }
 
 export function log(level: string, ...contents: any[]) {
-  const time = new Date().toLocaleTimeString()
+  const date = new Date()
+  const day = date.getDate().toString().padStart(2, '0')
+  const month = (date.getMonth() + 1).toString().padStart(2, '0')
+  const hours = date.getHours().toString().padStart(2, '0')
+  const minutes = date.getMinutes().toString().padStart(2, '0')
+  const seconds = date.getSeconds().toString().padStart(2, '0')
+  const fmtDate = `${day}/${month} ${hours}:${minutes}:${seconds}`
+
   let str = ''
-  str += chalk.gray(time) + ' ['
+  str += chalk.gray(fmtDate) + ' ['
   switch (level.toLowerCase()) {
     case 'error':
       str += chalk.redBright(level)
